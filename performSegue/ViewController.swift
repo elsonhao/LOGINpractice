05af09f6b3beb8f575da8cb2bde77320c1f0b9a0
Elson <huangyuhao@huangyuhaodeMacBook-Pro.local>
2017-02-22 15:57:15 +0800

    判斷密碼正確才進入下一頁並傳遞資料

--- performSegue/ViewController.swift
+++ performSegue/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var account: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    var acountCurrent = "123"
+    var acountPassword = "12345"
+    
+    @IBAction func loginin(_ sender: Any) {
+        guard account.text == acountCurrent && password.text == acountPassword else {
+            print("wrong")
+            return
+        }
+        
+        let but = sender as! UIButton
+        
+        performSegue(withIdentifier: "goNext", sender: but.currentTitle)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let secondController = segue.destination as! SecondViewController
+        
+        let but2 = sender as! String
+        
+        
+        secondController.name = but2
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
